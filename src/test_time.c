cb61016be4eda12d9b147414d9b4432acc750e29
kimtaeyoon2003122 <k0278kim@gmail.com>
2025-08-20 15:15:38 +0900

    fix: cpucycles 추가

--- src/test_time.c
+++ src/test_time.c
@@ -13,12 +13,10 @@
 
 long long cpucycles(void)
 {
-	// unsigned long long result;
-	// asm volatile(".byte 15;.byte 49;shlq $32,%%rdx;orq %%rdx,%%rax"
-	// 	: "=a" (result) ::  "%rdx");
-	// return result;
-
-	return mach_absolute_time();
+    unsigned long long result;
+    asm volatile(".byte 15;.byte 49;shlq $32,%%rdx;orq %%rdx,%%rax"
+        : "=a" (result) ::  "%rdx");
+    return result;
 }
 
 void
